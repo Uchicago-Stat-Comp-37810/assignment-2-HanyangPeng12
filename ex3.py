0d98bc174b846470dbc290876df284fa65872d19
HanyangPeng12 <43788597+HanyangPeng12@users.noreply.github.com>
2018-10-20 14:46:49 -0500

    Add some coments in ex3.py

--- ex3.py
+++ ex3.py
@@ -1,26 +1,26 @@
-print("I will now count my chickens:")
+print("I will now count my chickens:") #this will print what showed in the "" part
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25 + 30 / 6) #print the number of hens, by calculate the answer of 25+30/6
+print("Roosters", 100 - 25 * 3 % 4) #print the number of roosters, by calculate the answer of 100-25*3%4
 
-print("Now I will count the eggs:")
+print("Now I will count the eggs:") #this will print what showed in the "" part
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #print the answer of the calculation 3+2+1-5+4%2-1/4+6
 
-print("Is it true that 3 + 2 < 5 - 7?")
+print("Is it true that 3 + 2 < 5 - 7?") #this will print what showed in the "" part
 
-print(3 + 2 < 5 - 7)
+print(3 + 2 < 5 - 7) #print whether 3+2<5-7, if it is true, return true, otherwise return false
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print("What is 3 + 2?", 3 + 2) #pint the "" part and the answer of 3+2
+print("What is 5 - 7?", 5 - 7) #pint the "" part and the answer of 5-7
 
-print("Oh, that's why it's False.")
+print("Oh, that's why it's False.") #this will print what showed in the "" part
 
-print("How about some more.")
+print("How about some more.") #this will print what showed in the "" part
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater?", 5 > -2) #print the "" part and whether 5>-2
+print("Is it greater or equal?", 5 >= -2) #print the "" part and whether 5>=-2
+print("Is it less or equal?", 5 <= -2) #print the "" part and whether 5<=-2
 
 ################# Write something I need to calculate ##################
 print("let's calculate 0.25^50")

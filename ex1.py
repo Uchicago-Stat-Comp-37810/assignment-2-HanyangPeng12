da2ff96ab34b0ff2f23782844883541fd188bcac
HanyangPeng12 <43788597+HanyangPeng12@users.noreply.github.com>
2018-10-22 10:12:30 -0500

    change some code in ex1 and ex4

--- ex1.py
+++ ex1.py
@@ -1,10 +1,13 @@
 # STAT 37810 Assignment 2
 # Author: Hanyang Peng
 
-print("Hello World!")
-print("Hello Again")
-print("I like typing this.")
-print("This is fun.")
-print('Yay! Printing.')
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
+#print("Hello World!")
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+
+print("another line")
+#what we write after '#' will not be showed in the output
--- ex4.py
+++ ex4.py
@@ -1,22 +1,23 @@
 # STAT 37810 Assignment 2
 # Author: Hanyang Peng
 
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
-cars_not_driven = cars - drivers
-cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_driven
+cars = 100 #set the start value of cars
+space_in_a_car = 4.0 #set the start value of space in a car
+drivers = 30 #set the start value of the number of drivers
+passengers = 90 #set the start value of the number of passengers
+cars_not_driven = cars - drivers #calculate the number of cars which don't have drivers
+cars_driven = drivers #calculate the number of cars which have drivers
+carpool_capacity = cars_driven * space_in_a_car #calculate the carpool capacity, 
+#which means how many space it can provide for all those driven cars
+average_passengers_per_car = passengers / cars_driven #calculate the average passengers for each car
 # The error mentioned by the writer when it happened in the first time he write
 #is because he didn't define the variable 'car_pool_capacity', maybe he just type
 #the 'carpool_capacity' in the wrong way.
 
-print("There are", cars, "cars available.")
-print("There are only", drivers, "drivers available.")
-print("There will be", cars_not_driven, "empty cars today.")
-print("We can transport", carpool_capacity, "people today.")
-print("We have", passengers, "to carpool today.")
-print("We need to put about", average_passengers_per_car,
-      "in each car.")
+print("There are", cars, "cars available.") #print out how many cars are available
+print("There are only", drivers, "drivers available.") #print out how many drivers are available
+print("There will be", cars_not_driven, "empty cars today.") #print out how many cars are not driven
+print("We can transport", carpool_capacity, "people today.") #print out how many people we can drive today
+print("We have", passengers, "to carpool today.") #print out howmany passengers we need to drive
+print("We need to put about", average_passengers_per_car, 
+      "in each car.") #print out how many people will be in one car
